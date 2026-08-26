b9fa3f05a744f9f15ba98662e80d9af560859942
saikiran poka <saikiranpoka3@gmail.com>
2026-08-26 15:33:18 +0530

    module ecr is created

---
+++ modules/ecr/main.tf
@@ -0,0 +1,38 @@
+resource "aws_ecr_repository" "main" {
+  name                 = var.repository_name
+  image_tag_mutability = var.image_tag_mutability
+
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+
+  encryption_configuration {
+    encryption_type = "KMS"
+    kms_key         = var.kms_key_arn
+  }
+
+  tags = var.tags
+}
+
+resource "aws_ecr_lifecycle_policy" "main" {
+  repository = aws_ecr_repository.main.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep latest images"
+
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = var.images_to_keep
+        }
+
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
---
+++ modules/ecr/output.tf
@@ -0,0 +1,15 @@
+output "repository_name" {
+  value = aws_ecr_repository.this.name
+}
+
+output "repository_arn" {
+  value = aws_ecr_repository.this.arn
+}
+
+output "repository_url" {
+  value = aws_ecr_repository.this.repository_url
+}
+
+output "registry_id" {
+  value = aws_ecr_repository.this.registry_id
+}
